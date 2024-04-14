f48dfb6a7fbbbd731b8033baa383d0bd5580b4f7
gatfield <gatfield@umich.edu>
2024-04-14 19:16:02 -0400

    cahnges

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-function sendToPage() {
-    if (this.id === 'head') {
-        window.location.href = 'headphones.html';
-    } else if (this.id === 'tablet') {
-        window.location.href = 'tablets.html';
-    } else if (this.id === 'laptop') {
-        window.location.href = 'laptops.html';
-    } else {
-        return;
+var buttons = document.querySelectorAll('.button');
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var id = button.id;
+            if (id === 'head') {
+                window.location.href = 'headphones.html';
+            } else if (id === 'tablet') {
+                window.location.href = 'tablets.html';
+            } else if (id === 'laptop') {
+                window.location.href = 'laptops.html';
+            }
+        });
     }
-}
+);
