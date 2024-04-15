675da534ef19bd2aac621e1cf2409fb6818f5765
gatfield <gatfield@umich.edu>
2024-04-14 22:02:39 -0400

    cahnges

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ var buttons = document.querySelectorAll('.button');
         });
     }
 );
+
+// var buttons2 = document.querySelectorAll('#button2');
+//     buttons2.forEach(function(button) {
+//         button.addEventListener('click', function() {
+//             var id = button.id;
+//             if (id === 'head') {
+//                 window.location.href = 'headphones.html';
+//             } else if (id === 'tablet') {
+//                 window.location.href = 'tablets.html';
+//             } else if (id === 'laptop') {
+//                 window.location.href = 'laptops.html';
+//             }
+//         });
+//     }
+// );
